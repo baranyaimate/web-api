93e71c4f85bc1d4166fe61b3a8c7397f30ac6641
Baranyai Máté <mateb06@gmail.com>
2022-09-29 10:28:56 +0200

    Day #9

--- web-api/Models/DTO/AddressDto.cs
+++ web-api/Models/DTO/AddressDto.cs
@@ -4,17 +4,24 @@ namespace web_api.Models.DTO;
 
 public class AddressDto
 {
-    [JsonProperty("country")] public virtual string Country { get; set; } = string.Empty;
+    [JsonProperty("country")]
+    public virtual string Country { get; set; } = string.Empty;
 
-    [JsonProperty("city")] public virtual string City { get; set; } = string.Empty;
+    [JsonProperty("city")]
+    public virtual string City { get; set; } = string.Empty;
 
-    [JsonProperty("postcode")] public virtual string Postcode { get; set; } = string.Empty;
+    [JsonProperty("postcode")]
+    public virtual string Postcode { get; set; } = string.Empty;
 
-    [JsonProperty("state")] public virtual string State { get; set; } = string.Empty;
+    [JsonProperty("state")]
+    public virtual string State { get; set; } = string.Empty;
 
-    [JsonProperty("streetName")] public virtual string StreetName { get; set; } = string.Empty;
+    [JsonProperty("streetName")]
+    public virtual string StreetName { get; set; } = string.Empty;
 
-    [JsonProperty("streetNumber")] public virtual string StreetNumber { get; set; } = string.Empty;
+    [JsonProperty("streetNumber")]
+    public virtual string StreetNumber { get; set; } = string.Empty;
 
-    [JsonProperty("userId")] public virtual int UserId { get; set; }
+    [JsonProperty("userId")]
+    public virtual int UserId { get; set; }
 }
